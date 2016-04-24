1b26e3efcf3e3c7b3540f0f9c337d9429075c4d0
Saeed_rz <saeed.rezaei.sa@gmail.com>
2016-04-24 17:24:00 +0430

    Convert Html to String with Simple Extension
    to show web page or text with html tag like <b> ...., use this extension

---
+++ HTML2StringExtension.swift
@@ -0,0 +1,27 @@
+//
+//  HTML2String.swift
+//  Delion
+//
+//  Created by Saeed Rezaei on 3/10/16.
+//  Copyright © 2016 ILASoftware. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    var html2AttributedString: NSAttributedString? {
+        guard
+            let data = dataUsingEncoding(NSUTF8StringEncoding)
+            else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType,NSCharacterEncodingDocumentAttribute:NSUTF8StringEncoding], documentAttributes: nil)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            return  nil
+        }
+    }
+    var html2String: String {
+        return html2AttributedString?.string ?? ""
+    }
+}
